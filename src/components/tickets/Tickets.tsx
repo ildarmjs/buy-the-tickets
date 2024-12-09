c741846063a7465c5b63c98ed83b5d3ff28ec7e9
Ildar <ildar.1795@gmail.com>
2024-12-09 15:49:59 +0400

    feat: added logic for filters from redux

--- src/components/tickets/Tickets.tsx
+++ src/components/tickets/Tickets.tsx
@@ -5,10 +5,20 @@ import TicketsCard from './tickets-card/TicketsCard'
 
 const Tickets: FC = () => {
 	const tickets = useSelector((state: RootState) => state.tickets.tickets)
+	const selectedFilters = useSelector((state: RootState) => state.filters)
+
+	const filteredTickets = tickets.filter(ticket => {
+		if (selectedFilters.all) return true
+		if (selectedFilters.noStops && ticket.stops === 0) return true
+		if (selectedFilters.oneStop && ticket.stops === 1) return true
+		if (selectedFilters.twoStops && ticket.stops === 2) return true
+		if (selectedFilters.threeStops && ticket.stops === 3) return true
+		return false
+	})
 
 	return (
 		<div className='col-span-3 col-start-3'>
-			{tickets.map(ticket => (
+			{filteredTickets.map(ticket => (
 				<TicketsCard ticket={ticket} key={ticket.id} />
 			))}
 		</div>
