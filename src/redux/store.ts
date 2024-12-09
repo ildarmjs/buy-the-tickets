a2d3bf823eaea37f63b6411d9fea492935061fcb
Ildar <ildar.1795@gmail.com>
2024-12-09 15:48:37 +0400

    feat: added reducer for filters

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ticketsReducer from './ticketsSlice/ticketsSlice'
+import filtersReducer from './filtersSlice/filtersSlice'
 
 export const store = configureStore({
 	reducer: {
-		tickets: ticketsReducer
+		tickets: ticketsReducer,
+		filters: filtersReducer,
 	},
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
