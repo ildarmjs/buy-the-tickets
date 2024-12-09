7c7628aa512027c59faed087d3da32dc15d67839
Ildar <ildar.1795@gmail.com>
2024-12-09 15:19:23 +0400

    feat: added components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react'
+import Tickets from './components/tickets/Tickets'
+import Container from './components/container/Container'
+import Filters from './components/filters/Filters'
 
 const App: FC = () => {
-	return <div>App</div>
+	return (
+		<Container>
+			<div className='grid grid-cols-5 grid-rows-5 gap-4 mt-10'>
+				<Filters />
+				<Tickets />
+			</div>
+		</Container>
+	)
 }
 
 export default App
