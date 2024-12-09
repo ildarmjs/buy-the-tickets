efc466f03a46c9fb2010e9ee70872963ec167331
Ildar <ildar.1795@gmail.com>
2024-12-09 15:49:53 +0400

    feat: added logic for filters from redux

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,6 +1,16 @@
 import { FC } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../redux/store'
+import { setFilter } from '../../redux/filtersSlice/filtersSlice'
 
 const Filters: FC = () => {
+	const dispatch = useDispatch()
+	const selectedFilters = useSelector((state: RootState) => state.filters)
+
+	const handleFilterChange = (filter: string) => {
+		dispatch(setFilter(filter))
+	}
+
 	return (
 		<div className='col-span-2'>
 			<div className='sticky top-5 bg-slate-100 rounded-sm border border-gray-300 h-[auto] p-4'>
@@ -13,35 +23,50 @@ const Filters: FC = () => {
 					</div>
 				</div>
 				<div>
-					<h3>Колличество пересадок</h3>
+					<h3>Количество пересадок</h3>
 					<div>
-						<input type='checkbox' />
+						<input
+							type='checkbox'
+							checked={selectedFilters.all}
+							onChange={() => handleFilterChange('all')}
+						/>
 						<label>Все</label>
 					</div>
 					<div>
-						<input type='checkbox' />
+						<input
+							type='checkbox'
+							checked={selectedFilters.noStops}
+							onChange={() => handleFilterChange('noStops')}
+						/>
 						<label>Без пересадок</label>
 					</div>
 					<div>
-						<input type='checkbox' />
+						<input
+							type='checkbox'
+							checked={selectedFilters.oneStop}
+							onChange={() => handleFilterChange('oneStop')}
+						/>
 						<label>1 пересадка</label>
 					</div>
 					<div>
-						<input type='checkbox' />
-						<label>2 пересадка</label>
+						<input
+							type='checkbox'
+							checked={selectedFilters.twoStops}
+							onChange={() => handleFilterChange('twoStops')}
+						/>
+						<label>2 пересадки</label>
 					</div>
 					<div>
-						<input type='checkbox' />
-						<label>3 пересадка</label>
+						<input
+							type='checkbox'
+							checked={selectedFilters.threeStops}
+							onChange={() => handleFilterChange('threeStops')}
+						/>
+						<label>3 пересадки</label>
 					</div>
-				</div>
-				<div>
-					<button>Применить</button>
-					<button>Сбросить</button>
 				</div>
 			</div>
 		</div>
 	)
 }
-
 export default Filters
