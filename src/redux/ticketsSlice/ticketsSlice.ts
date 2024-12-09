d520df5472ef410afd367431c326df35543495ce
Ildar <ildar.1795@gmail.com>
2024-12-09 16:00:18 +0400

    refactor: deleted import

--- src/redux/ticketsSlice/ticketsSlice.ts
+++ src/redux/ticketsSlice/ticketsSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
 import dataTickets from '../../data/tickets.json'
 import { TypeTickets } from '../../types/types.tickets'
 export interface TicketsState {
